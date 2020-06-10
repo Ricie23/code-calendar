08ebfd8e1b799a16179ffd79106ccf1f8f5f52d5
Rich <rrobertson@tentcraft.com>
2020-06-10 10:45:34 -0400

    made calendar.vue but not sure hwo to import it to main page

--- front-end/code-calander/src/components/MainPage.vue
+++ front-end/code-calander/src/components/MainPage.vue
@@ -8,12 +8,27 @@
       <p>All of our stuff should go between these template tags surrounding this code</p>
       <p>Styles for everything on this page can be added below in the style tag</p>
       <p>inside of the script below is where we can add variables and lifecycle hooks</p>
-      <vue-cal style="height: 250px" />
+  <div class="calendar">
+      <div class="calendar-header">
+          <i class="fa fa-fw fa-chevron-left" @click="subtractMonth"></i>
+          <h4>{{month + ' - ' + year}}</h4>
+          <i class="fa fa-fw fa-chevron-right" @click="addMonth"></i>
+      </div>
+      <ul class="weekdays">
+        <li v-for="day in days" :key="day"></li>
+      </ul>
+      <ul class="dates">
+        <li v-for="blank in firstDayOfMonth" :key="blank">&nbsp;</li>
+        <li v-for="date in daysInMonth" 
+        :class="{'current-day': date == initialDate &amp;&amp; month == initialMonth && year == initialYear}" :key="date" >;
+        <span></span>
+        </li>
+      </ul>
+    </div>    
   </div> 
 </template>
 
 <script>
-
 
 export default {
   name: 'HelloWorld',
