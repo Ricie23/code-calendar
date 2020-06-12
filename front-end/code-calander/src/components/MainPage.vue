bcdb4d99c94463b325949564f742aa74954ea6e7
Rich <rrobertson@tentcraft.com>
2020-06-12 18:07:19 -0400

    rough draft of styled wireframe

--- front-end/code-calander/src/components/MainPage.vue
+++ front-end/code-calander/src/components/MainPage.vue
@@ -3,12 +3,19 @@
   <div class="giant-container" style="border: 1px solid red; height: 100vh;">
     <div class="menu">
       <img src="../media/code_calanderV1.png" width="20%" style="float: left; margin:30px; margin-top:0px; "/>
-      <ul>
+      <ul style="margin-right:40px">
         <li>About</li>
         <li>Contact</li>
       </ul>
     </div >
-    <p>Styles for everything on this page can be added below in the style tag</p>
+    <div style="margin:40px; margin-top:100px;">
+      <p>Maybe something written here about clicking on the day to show information?</p>
+    </div>
+  
+    <div class="calendar">
+<h1>Calendar goes here</h1>
+    </div>
+
   </div>  
 </template>
 <script>
@@ -39,9 +46,16 @@ h1{
 .menu li{
   display: inline; 
   text-decoration: none;
+  float: right;
 margin:30px;
 }
 .menu{
   margin-top: 30px;
+}
+.calendar{
+margin:150px;
+  text-align: center;
+  border: 1px solid;
+  border-color: green
 }
 </style>
